b8d1ffbf09f77e9664ee606c399e1795707eead9
QWOFTQ <rkdcofls3890@naver.com>
2023-12-12 22:07:55 +0900

    urldelete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React from "react"
 
-import Box from "@mui/material/Box";
+import Box from "@mui/material/Box"
 
 function Footer() {
-	return (
-		<footer>
-			<Box sx={{ height: "5vh" }}></Box>
-		</footer>
-	);
+  return (
+    <footer>
+      <Box sx={{ height: "5vh" }}></Box>
+      <p align="center">©중부대학교 강김홍차</p>
+    </footer>
+  )
 }
 
-export default Footer;
+export default Footer
